431674ff576383312ff98054d55f2755a9f6a09a
Milojez <74306753+Milojez@users.noreply.github.com>
2022-05-06 12:04:53 +0200

    Update power_used.py

--- power_used.py
+++ power_used.py
@@ -1,4 +1,4 @@
-from re import A
+
 import numpy as np
 #data
 
@@ -8,14 +8,13 @@ rho = 1.225
 FOM = 0.75
 
 V_climb = 5
-v_h = 30
 
 #rotors data
 #concept 1
 rotor_d = 1.5 #[m]
 blades_number = 3
 rotors_number = 4
-Area = np.pi *(rotor_d/2)**2 #[m^2] #propeler area
+one_rotor_area = np.pi *(rotor_d/2)**2 #[m^2] #propeler area
 #concept 2
 #concept 3
 #concept 4
@@ -31,17 +30,23 @@ lst_new_prop = []
 i= 0
 for i in range(0,5):
     
-    #HOVER POWER
+    #----HOVER POWER------
     T = mass*g #thrust
-    P_hover = FOM * np.sqrt(T**3/(rho*Area)) #[W]
+    P_hover = FOM * np.sqrt(T**3/(rho*one_rotor_area)) #[W]
     # print(P_hover, "W")
-    print()
-    print(P_hover/1000, "kW")
-    print()
+    # print()
+    # print(P_hover/1000, "kW")
+    # print()
 
-    #CLIMB POWER
+    #------CLIMB/DESCEND POWER----
     # P_climb = P_hover
+    v_i = np.sqrt(T/(2*rho*one_rotor_area/4))
+    v_h = np.sqrt((V_climb+v_i)*v_i)
+    print("v_h", v_h)
+
     P_climb = P_hover*(V_climb/(2*v_h) + np.sqrt((V_climb/(2*v_h))**2+1))
+    print(P_climb)
+    print(P_hover)
     P_descend =  P_hover*(V_climb/(2*v_h) - np.sqrt((V_climb/(2*v_h))**2-1))
 
     #mass of a motor
@@ -49,8 +54,8 @@ for i in range(0,5):
     #mass 0f a propeller
     m_prop = 1.1 *(rotor_d*(P_climb/1000/rotors_number)*np.sqrt(blades_number))**0.52
 
-    print( "rotor mass", m_motor)
-    print( "propeller mass", m_prop)
+    # print( "rotor mass", m_motor)
+    # print( "propeller mass", m_prop)
 
     #list of iterartions for different mass of rotors and propellers
     lst_new_motor = lst_new_motor + [m_motor]
@@ -62,7 +67,7 @@ for i in range(0,5):
         mass = mass + rotors_number*(m_motor+m_prop)
     else:
         mass = mass + rotors_number*(lst_new_prop[i] + lst_new_motor[i]) - rotors_number*(lst_new_prop[i-1] + lst_new_motor[i-1]) 
-    print("mass", mass)
+    # print("mass", mass)
     i=i+1
 
 
