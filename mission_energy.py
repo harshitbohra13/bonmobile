11bf6852e646b657a97359259c412e85d5820573
Milojez <74306753+Milojez@users.noreply.github.com>
2022-05-06 08:50:22 +0200

    mission power

---
+++ mission_energy.py
@@ -0,0 +1,13 @@
+
+#times
+
+t_hover =
+t_climb = 
+t_descend =
+
+t_cruise = 40/100
+
+
+
+# Total energy
+E = P_hover*t_hover + P
--- power_used.py
+++ power_used.py
@@ -1,46 +1,63 @@
-#times for particular phases
-# from matplotlib.font_manager import _Weight
-
 import numpy as np
-# t_climb =
-# t_descend = 
-# t_cruise = 40/100 #range over crusie speed
-
 #data
+
 mass = 700 #[kg] #aircraft mass
 g = 9.8 
 rho = 1.225
-rotor_d = 1.5 #[m]
+FOM = 0.75
 
 #rotors data
+#concept 1
+rotor_d = 1.5 #[m]
 blades_number = 3
 rotors_number = 4
 Area = np.pi *(rotor_d/2)**2 #[m^2] #propeler area
-FOM = 0.75
+#concept 2
+#concept 3
+#concept 4
 
-# T = mass*g #thrust [N]
 
-#iteration 
+
+
+#list definition
+lst_new_motor = []
+lst_new_prop = []
+
+#iteration for mass and power
 i= 0
 for i in range(0,5):
-    i=i+1
+    
+    #HOVER POWER
     T = mass*g
     P_hover = FOM * np.sqrt(T**3/(rho*Area)) #[W]
     # print(P_hover, "W")
+    print()
     print(P_hover/1000, "kW")
+    print()
 
+    #CLIMB POWER
     P_climb = P_hover
 
-    m_motor = (0.188*P_climb/1000 +5.836)/rotors_number #power must be given in kW article figure
+    #mass of a motor
+    m_motor = (0.188*P_climb/1000 +5.836)/rotors_number #power in the equation must be given in kW thus P_climb/1000 (article figure)
+    #mass 0f a propeller
     m_prop = 1.1 *(rotor_d*(P_climb/1000/rotors_number)*np.sqrt(blades_number))**0.52
+
     print( "rotor mass", m_motor)
     print( "propeller mass", m_prop)
-    
-    m_new_motor = m_motor
-    m_new_prop = m_prop
 
-    mass = mass + rotors_number*(m_motor+m_prop)
+    #list of iterartions for different mass of rotors and propellers
+    lst_new_motor = lst_new_motor + [m_motor]
+    lst_new_prop = lst_new_prop+ [m_prop]
+
+    
+    #update aircraft i ==0 and then i> exchange the rotors and propellors
+    if i ==0:
+        mass = mass + rotors_number*(m_motor+m_prop)
+    else:
+        mass = mass + rotors_number*(lst_new_prop[i] + lst_new_motor[i]) - rotors_number*(lst_new_prop[i-1] + lst_new_motor[i-1]) 
     print("mass", mass)
+    i=i+1
 
 
 
