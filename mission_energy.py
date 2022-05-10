49484b6f5fdc276c44266de0e1344c715b2558a9
Milojez <74306753+Milojez@users.noreply.github.com>
2022-05-10 15:00:34 +0200

    hover update

--- mission_energy.py
+++ mission_energy.py
@@ -1,16 +1,16 @@
-from power_concept1 import P_hover, P_climb
-#times
+# from power_concept1 import P_hover, P_climb
+# #times
 
-t_hover =
-t_climb = 
-t_descend =
+# t_hover =
+# t_climb = 
+# t_descend =
 
-t_cruise = 40/100
+# t_cruise = 40/100
 
 
 
-# Total energy
-E =  2 * P_hover*t_hover + P_climb*t_climb + P_cruise*t_cruise + P_descend * t_descend
-print("Total mission energy:", E, "J")
+# # Total energy
+# E =  2 * P_hover*t_hover + P_climb*t_climb + P_cruise*t_cruise + P_descend * t_descend
+# print("Total mission energy:", E, "J")
 
-#
+# #
