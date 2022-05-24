3a97d95fbd8699321200d9217e19061d03bae8eb
Milojez <74306753+Milojez@users.noreply.github.com>
2022-05-24 15:12:26 +0200

    batt

--- Final_Design/Battery_mass.py
+++ Final_Design/Battery_mass.py
@@ -1,1 +1,2 @@
 Total_Battery_mass = 0
+Max_Total_thrust = 0
--- Final_Design/Sup_struct_mass.py
+++ Final_Design/Sup_struct_mass.py
@@ -1,17 +1,12 @@
-
-
 from sqlalchemy import null
+from Battery_mass import Max_Total_thrust
+from configuration_and_materials import Rotor_number
+
+# Total_Supporting_structure_mass = 0
 
 
-Total_Supporting_structure_mass = 0
-
-#Structures data
-Rotor_number = 12
-
-#Maximal total thrust from engines
-Max_Total_thrust = 0
 #Thrust per rotor
 Max_thrust_per_rotor = Max_Total_thrust/Rotor_number
 
-def sup_struct_mass():
+def sup_struct_mass(Rotor_number, ):
     return null
