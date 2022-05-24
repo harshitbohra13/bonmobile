c8f4c414e9245f0204bbd1a328ea192837f870cc
Milojez <74306753+Milojez@users.noreply.github.com>
2022-05-24 17:08:17 +0200

    Update Sup_struct_mass.py

--- Final_Design/Sup_struct_mass.py
+++ Final_Design/Sup_struct_mass.py
@@ -8,5 +8,16 @@ from configuration_and_materials import Rotor_number
 #Thrust per rotor
 Max_thrust_per_rotor = Max_Total_thrust/Rotor_number
 
+def Strut_1(Fuselage_width, fuselage_rotor_clearance, rotor_radius, safety_factor, T_max, r_outer, Density_1):
+    Length_1 = (Fuselage_ width/2) * np.sqrt(2) + np.sqrt(2) * (fuselage_rotor_clearance+rotor_radius)
+    max_stress = y_strength_mat1/safety_factor
+    M = 0.25* T_max * Length_1
+    I = (M*r_outer)/(max_stress)
+    r_inner = (r_outer**4 - (4/np.pi)*I)**0.25
+    Area_1 = np.pi *(r_outer**2 - r_inner **2)
+    Strut_1_mass = Area_1 * Length_1 * Density_1
+    return Strut_1_mass
+    # print("max stress", max_stress/(10**6), "MPa")
+    
 def sup_struct_mass(Rotor_number, ):
     return null
