b2abc0ed74682bca7b09a97e311037f0a92593fe
miordan <104829008+miordan@users.noreply.github.com>
2022-05-25 09:05:08 +0200

    Added part for structure 2

--- Final_Design/Sup_struct_mass.py
+++ Final_Design/Sup_struct_mass.py
@@ -1,6 +1,7 @@
 from sqlalchemy import null
 from Battery_mass import Max_Total_thrust
-from configuration_and_materials import Rotor_number
+from configuration_and_materials import *
+from 
 
 # Total_Supporting_structure_mass = 0
 
@@ -21,3 +22,14 @@ def Strut_1(Fuselage_width, fuselage_rotor_clearance, rotor_radius, safety_facto
     
 def sup_struct_mass(Rotor_number, ):
     return null
+
+def Strut_2(Fuselage_width, fuselage_rotor_clearance, rotor_radius, safety_factor, T_max, r_outer, Density_2):
+    Length_2 = (Fuselage_ width/2) * np.sqrt(2) + np.sqrt(2) * (fuselage_rotor_clearance+rotor_radius)
+    max_stress = y_strength_mat2/safety_factor
+    M = 0.25* T_max * Length_2
+    I = (M*r_outer)/(max_stress)
+    r_inner = (r_outer**4 - (4/np.pi)*I)**0.25
+    Area_1 = np.pi *(r_outer**2 - r_inner **2)
+    Strut_1_mass = Area_2 * Length_2 * Density_2
+    return Strut_2_mass
+    # print("max stress", max_stress/(10**6), "MPa")
