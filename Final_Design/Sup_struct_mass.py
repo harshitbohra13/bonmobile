c78979f1f1ee945b2e034b25a547dda549e809c0
miordan <104829008+miordan@users.noreply.github.com>
2022-05-25 10:43:45 +0200

    Integration of code

--- Final_Design/Sup_struct_mass.py
+++ Final_Design/Sup_struct_mass.py
@@ -1,19 +1,22 @@
 from sqlalchemy import null
 from Battery_mass import Max_Total_thrust
-from configuration_and_materials import *
+from configuration_and_materials import configure, Aluminiumbadboy
 from Fuselage_mass import *
 from Motor_mass import *
+import numpy as np
 
 # Total_Supporting_structure_mass = 0
 
 
 #Thrust per rotor
-Max_thrust_per_rotor = Max_Total_thrust/Rotor_number
+def thrustperrotor(Total_T, Rotor_number):
+    Max_thrust_per_rotor = Max_Total_thrust/Rotor_number
+    return(Max_thrust_per_rotor)
 
-def Strut_1(Fuselage_width, fuselage_rotor_clearance, rotor_radius, safety_factor, T_max, r_outer, Density_1):
-    Length_1 = (Fuselage_width/2) * np.sqrt(2) + np.sqrt(2) * (fuselage_rotor_clearance+rotor_radius)
-    max_stress = y_strength_mat1/safety_factor
-    M = 0.25* T_max * Length_1
+def Strut_1(bon, Fuselage_width, rotor_radius, safety_factor, T_max, r_outer, Density_1):
+    Length_1 = (Fuselage_width/2) * np.sqrt(2) + np.sqrt(2) * (bon.fuselage_rotor_clearance+rotor_radius)
+    max_stress = Aluminiumbadboy.y_strength_mat/safety_factor
+    M = 0.25* T_max * Length_1 + 2*T_max* bon.rotor_to_rotor_clearance 
     I = (M*r_outer)/(max_stress)
     r_inner = (r_outer**4 - (4/np.pi)*I)**0.25
     Area_1 = np.pi *(r_outer**2 - r_inner **2)
@@ -24,10 +27,10 @@ def Strut_1(Fuselage_width, fuselage_rotor_clearance, rotor_radius, safety_facto
 def sup_struct_mass(Rotor_number, ):
     return null
 
-def Strut_2(Fuselage_width, fuselage_rotor_clearance, rotor_radius, safety_factor, T_max, r_outer, Density_2):
-    Length_2 = 1
-    max_stress = y_strength_mat2/safety_factor
-    M = 0.25* T_max * Length_2
+def Strut_2(bon, safety_factor, T_max, r_outer, Density_2):
+    Length_2 = 2* bon.rotor_radius + bon.rotor_to_rotor_clearance
+    max_stress = Aluminiumbadboy.y_strength_mat/safety_factor
+    M =  (T_max * Length_2)/12
     I = (M*r_outer)/(max_stress)
     r_inner = (r_outer**4 - (4/np.pi)*I)**0.25
     Area_2 = np.pi *(r_outer**2 - r_inner **2)
