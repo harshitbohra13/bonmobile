1f4063b60d8925fc2da11fafc005247af2f92708
ipvanalkemade <ipvanalkemade@student.tudelft.nl>
2022-06-07 14:48:42 +0200

    f and m root

--- Final_Design/Skid_mass.py
+++ Final_Design/Skid_mass.py
@@ -58,4 +58,26 @@ M_z_2 = M_z * cos(alpha)
 M_y_1 = M_y * cos(alpha)
 M_y_2 = M_y * sin(alpha)
 
+#----------stresses in cross-section ----------
+
+#sigma_normal = F_z_end / (np.pi * t**2 + 2 * t * r_in_rod)
+#sigma_shear = F_x_end * (t + r_in_rod) * 0.5 * np.pi * (t**2 + 2 * t * r_in_rod) / (np.pi/4 * (t**4 + 4 * t**3 * r_in_rod + 6*t**2 * r_in_rod**2 + 4*t*r_in_rod**3) * t)
+#sigma_torsion = T * (t + r_in_rod) / ( np.pi/2 * (t**4 + 4 * t**3 * r_in_rod + 6*t**2 * r_in_rod**2 + 4*t*r_in_rod**3))
+#sigma_bending = moment * (t + r_in_rod) / (np.pi/4 * (t**4 + 4 * t**3 * r_in_rod + 6*t**2 * r_in_rod**2 + 4*t*r_in_rod**3))
+
+def sigma_normal(t):
+    return F_z_end / (np.pi * t**2 + 2 * t * r_in_rod)
+
+def sigma_shear(t):
+    return F_x_end * (t + r_in_rod) * 0.5 * np.pi * (t**2 + 2 * t * r_in_rod) / (np.pi/4 * (t**4 + 4 * t**3 * r_in_rod + 6*t**2 * r_in_rod**2 + 4*t*r_in_rod**3) * t)
+
+def sigma_torsion(t):
+    return T * (t + r_in_rod) / ( np.pi/2 * (t**4 + 4 * t**3 * r_in_rod + 6*t**2 * r_in_rod**2 + 4*t*r_in_rod**3))
+
+def sigma_bending(t):
+    return moment * (t + r_in_rod) / (np.pi/4 * (t**4 + 4 * t**3 * r_in_rod + 6*t**2 * r_in_rod**2 + 4*t*r_in_rod**3))
+
+
+
+
 
