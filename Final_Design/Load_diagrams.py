6d4c0f5db490ca03db0eff8557dd619cf73c009f
Mickiboyhekkens <74193325+Mickiboyhekkens@users.noreply.github.com>
2022-06-10 08:35:21 +0200

    poep
    Poepe

--- Final_Design/Load_diagrams.py
+++ Final_Design/Load_diagrams.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 class LoadDiagrams():
 
-    def __init__(self, disk_loading, W, omega):
+    def __init__(self, R, W, omega, P_a):
         self.rho = 1.225
-        self.disk_loading = disk_loading
+        self.R_rotor = R
         self.n_rotors = 12
         self.W = W*9.81
 
-        self.A_T = self.W / self.disk_loading
-        self.A_rotor = self.A_T/self.n_rotors
-        self.R_rotor = np.sqrt(self.A_rotor/np.pi)
+        self.A_rotor = self.R_rotor**2 * np.pi
+        self.A_T = self.A_rotor * 12
+        self.disk_loading = self.W/self.A_T
 
         self.omega = omega*0.1047198
         self.v_h = np.sqrt(self.disk_loading/(2*self.rho))
@@ -19,6 +19,8 @@ class LoadDiagrams():
         self.V_C = 100/3.6
         self.P_h = 2*self.rho*self.A_T*self.v_h**3
         self.k = 1.15
+        self.P_a = P_a
+        self.FM = 0.7
 
     def get_drag(self, V):
         D_c = 350
@@ -26,12 +28,18 @@ class LoadDiagrams():
         D = F * 0.5 * self.rho * V ** 2
         return D
 
+    def get_drag_horizontal(self, V_h):
+        D_h = 60
+        F = D_h / (0.5 * self.rho * 6 ** 2)
+        D = F * 0.5 * self.rho * V_h ** 2
+        return D
+
     def get_aoa(self, V):
         aoa = np.tanh(self.get_drag(V)/self.W)
         return aoa
 
     def get_parasitic_power(self, V):
-        return self.get_drag(V)*V
+        return self.get_drag(V)*V / self.FM
 
     def plot_aoa(self):
         V = np.arange(0, 100, 0.1)
@@ -137,26 +145,26 @@ class LoadDiagrams():
         mu = self.get_advance_ratio(V)
         K = 4.65
         Cd0 = 0.0085
-        sigma = self.rho/1.225
+        sigma = 0.1
         cp = (1+K*mu**2)*sigma*Cd0/8
-        P = cp*self.A_T* self.omega**3 * self.R_rotor ** 3
-        return P
+        P = cp*self.A_T* (self.omega*self.R_rotor) ** 3
+        return P / self.FM
 
     def get_power(self, V):
-        P_i = self.get_v_inflow(V)*self.get_thrust(V, 0)
-        P_p = self.get_parasitic_power(V)
+        P_i = self.get_v_inflow(V)*self.get_thrust(V, 0) / self.FM
+        P_p = self.get_parasitic_power(V) / self.FM
         P_profile = self.get_profile_power(V)
         return P_i + P_p + P_profile
 
+
     def plot_power_actual(self):
         V = np.arange(0, 200/3.6, 0.01)
-        P = [self.get_power(i)*self.k/12000 for i in V]
-        P_i = [self.get_v_induced(i)*self.get_thrust(i, 0)*self.k/12000 for i in V]
-        P_p = [self.get_advance_ratio(i)*self.omega*self.R_rotor*np.tan(self.get_aoa(i))*self.get_thrust(i, 0)*self.k/12000 for i in V]
+        P = [self.get_power(i)/12000 for i in V]
+        P_i = [self.get_v_induced(i)*self.get_thrust(i, 0)/12000 for i in V]
+        P_p = [self.get_advance_ratio(i)*self.omega*self.R_rotor*np.tan(self.get_aoa(i))*self.get_thrust(i, 0)/12000 for i in V]
         P_d = [self.get_parasitic_power(i)/12000 for i in V]
         P_prof = [self.get_profile_power(i)/12000 for i in V]
-        P_a = np.ones(len(V))*15
-
+        P_a = np.ones(len(V))*self.P_a
 
         plt.plot(V*3.6, P_a, label='Available power', c='black', linestyle='--')
         plt.plot(V*3.6, P, label='Total power required ', c='black')
@@ -169,10 +177,11 @@ class LoadDiagrams():
         plt.legend()
         plt.show()
 
+
     def performance(self):
         V = np.arange(0, 200/3.6, 0.1)
-        P = np.array([self.get_power(i)*self.k/12000 for i in V])
-        P_a = np.ones(len(V)) * 15
+        P = np.array([self.get_power(i)/12000 for i in V])
+        P_a = np.ones(len(V)) * self.P_a
 
         P_end = np.arange(0, min(P), 0.1)
         V_end = np.ones(len(P_end))*V[np.where(P==min(P))]
@@ -195,9 +204,9 @@ class LoadDiagrams():
         plt.plot(V * 3.6, P_a, label='Available Power', c='black', linestyle='--')
         plt.plot(V * 3.6, P, label='Total Power Required', c='black')
 
-        plt.plot(V_end*3.6, P_end, label=f'V max. endurance = {round(V_end[0]*3.6)} (m/s)')
-        plt.plot(V_ran*3.6, P_ran, label=f'V max. range = {round(V_ran[0]*3.6)} (m/s)', c='orange')
-        plt.plot(V_max*3.6, P_max, label=f'max. V attainable = {round(V_max[0]*3.6)} m/s', c='green')
+        plt.plot(V_end*3.6, P_end, label=f'V max. endurance = {round(V_end[0]*3.6)} (km/h)')
+        plt.plot(V_ran*3.6, P_ran, label=f'V max. range = {round(V_ran[0]*3.6)} (km/h)', c='orange')
+        plt.plot(V_max*3.6, P_max, label=f'max. V attainable = {round(V_max[0]*3.6)} (km/h)', c='green')
         plt.plot(V * 3.6, range, linestyle=':', c='orange')
 
         plt.legend()
@@ -214,15 +223,21 @@ class LoadDiagrams():
     def get_power_climb(self, V_c):
         P_c = ((V_c/(self.v_h*2)) + np.sqrt((V_c/(self.v_h*2))**2+1)) * self.P_h
         P_p = self.get_profile_power(0)
-        return P_c + P_p
+        P_d = V_c * self.get_drag_horizontal(V_c)
+        return P_c + P_p + P_d
 
     def get_power_descent(self, V_c):
-        return ((V_c/(self.v_h*2)) - np.sqrt((V_c/(self.v_h*2))**2-1)) * self.P_h + self.get_profile_power(0)
+        P_d = (-0.125* V_c + 0.974 - 1.372*V_c**2 -1.718*V_c**3 - 0.655*V_c**4)*self.P_h
+        P_p = self.get_profile_power(0)
+        P_drag = V_c * self.get_drag_horizontal(V_c)
+        return P_d + P_p + P_drag
 
     def get_thrust_climb(self, V_c):
         v_i = (-(V_c/(self.v_h*2)) + np.sqrt(((V_c/(self.v_h*2))**2)+1))*self.v_h
         T = 2*self.rho*self.A_T*(V_c+v_i)*v_i
-        return T
+        D = self.get_drag_horizontal(V_c)
+        print(D)
+        return T + D
 
     def get_inflow_climb(self, V_c):
         return (-(V_c/(self.v_h*2)) + np.sqrt(((V_c/(self.v_h*2))**2)+1))*self.v_h + V_c
@@ -242,20 +257,20 @@ class LoadDiagrams():
         plt.show()
 
     def plot_climb_descent(self):
-        def get_P_b(V_b):
-            return (-0.125* V_b + 0.974 - 1.372*V_b**2 -1.718*V_b**3 - 0.655*V_b**4)*self.P_h
-
-        V_c = np.arange(0, 20, 0.1)
-        V_b = np.arange(-20, 0, 0.1) / self.v_h
+        V_c = np.arange(0, 10, 0.1)
+        V_d = np.arange(-20, 0, 0.1) / self.v_h
+        V_t = np.arange(-20, 20, 0.1)
 
         P_c = [self.get_power_climb(i) / 12000 for i in V_c]
-        P_b = [get_P_b(i) / 12000 for i in V_b]
+        P_d = [self.get_power_descent(i)  / 12000 for i in V_d]
+        P_a = np.ones(len(V_t))*self.P_a
+
         plt.plot(V_c, P_c, label='Normal working state')
-        plt.plot(V_b*self.v_h, P_b, label='Vortex ring state')
+        plt.plot(V_d*self.v_h, P_d, label='Vortex ring state')
+        plt.plot(V_t, P_a, label='Power available', linestyle='--', c='black')
         plt.xlabel('$V_{c}$ and $V_{d}$')
         plt.ylabel('P (kW)')
         plt.legend()
-        #plt.plot(V_d, P_d)
         plt.show()
 
 
@@ -298,13 +313,20 @@ class LoadDiagrams():
 
 
 
-# Input: Disk Loading (N/m^2), Weight (kg), rpm
-diagram = LoadDiagrams(400, 1000, 1000)
-print(diagram.get_power_climb(0))
-#diagram.plot_power_actual()
-diagram.plot_climb_descent()
+# Input: Radius (m), Weight (kg), rpm, P_a
+diagram = LoadDiagrams(0.9, 1256.8, 2011.5, 26.163)
+
+diagram.program_input(0, 6)
+
+#print(diagram.get_power_climb(0))
+#print(diagram.get_power(0))
+diagram.plot_power_actual()
+#diagram.plot_climb_descent()
 #diagram.plot_climb_performance()
-#diagram.performance()
+diagram.performance()
+
+
+
 
 
 
--- Final_Design/Power_sensitivity.py
+++ Final_Design/Power_sensitivity.py
@@ -4,13 +4,16 @@ import seaborn as sns
 
 class PowerSensitivity():
 
-    def get_power(self, V, disk_loading, W, omega, rho, D_c):
+    def get_power(self, V, R, W, omega, rho, D_c):
         n_rotors = 12
         W = 9.81*W
+        R_rotor = R
 
-        A_T = W / disk_loading
-        A_rotor = A_T / n_rotors
-        R_rotor = np.sqrt(A_rotor / np.pi)
+        A_rotor = R_rotor ** 2 * np.pi
+        A_T = A_rotor * 12
+        disk_loading = W / A_T
+
+
         omega = omega * 0.1047198
         v_h = np.sqrt(disk_loading / (2 * rho))
         C_T = disk_loading / (rho * omega ** 2 * R_rotor ** 2)
@@ -21,7 +24,7 @@ class PowerSensitivity():
         k = 1.15
         K_profile = 4.65
         Cd0 = 0.0085
-        sigma = rho / 1.225
+        sigma = 0.1
 
 
         mu_x = V * np.cos(aoa) / (omega * R_rotor)
@@ -39,7 +42,7 @@ class PowerSensitivity():
         v_induced = inflow_ratio * omega * R_rotor - V * np.sin(aoa)
 
         T = 2 * rho * A_T * v_induced * np.sqrt(V ** 2 + (v_inflow ** 2))
-        P_induced = v_inflow*T*k
+        P_induced = v_inflow*T
         P_parasitic = D*V
         cp_profile = (1 + K_profile * mu_x ** 2) * sigma * Cd0 / 8
         P_profile = cp_profile * A_T * omega ** 3 * R_rotor ** 3
@@ -71,6 +74,82 @@ class PowerSensitivity():
         plt.ylabel('$P$ (kW)')
         plt.legend()
         plt.show()
+
+    def power_radius(self, R, W, omega, rho, D_c, P_a):
+        rpms = np.arange(1000, 4000, 500)
+        R = np.arange(0.5*R, 1.5*R, 0.01)
+        R_optimal_list = []
+
+        for rpm in rpms:
+             P = [self.get_power(0, i, W, rpm, rho, D_c)[0]/(12000*0.6) for i in R]
+             min_P = min(P)
+             R_optimal = R[P.index(min_P)]
+             R_optimal_list.append(R_optimal)
+             plt.plot(R, P, label=f'rpm={rpm}')
+             plt.plot(R_optimal, min_P, marker='o', c='r')
+
+        P_actual = np.ones(len(R)) * P_a
+        plt.plot(R, P_actual, c='black', linestyle='--', label='Available power')
+        plt.xlabel('Rotor radius')
+        plt.ylim([15,60])
+        plt.ylabel('Power required cruise')
+        plt.legend()
+        plt.show()
+
+        plt.plot(rpms, R_optimal_list)
+        plt.xlabel('rpm')
+        plt.ylabel('R rotor')
+        plt.show()
+
+    def power_rpm_final(self, R, W, omega, rho, D_c, P_a):
+        rpms = np.arange(0, 4000, 100)
+        radius = np.arange(0.7, 1.1, 0.1)
+        rpm_optimal_list = []
+
+        for R in radius:
+            P = [self.get_power(0, R, W, i, rho, D_c)[0] / (12000 * 0.7) for i in rpms]
+            min_P = min(P)
+            rpm_optimal = rpms[P.index(min_P)]
+            rpm_optimal_list.append(rpm_optimal)
+            plt.plot(rpms, P, label=f'R={round(R, 1)}')
+            plt.plot(rpm_optimal, min_P, marker='o', c='r')
+
+        P_actual = np.ones(len(rpms)) * P_a
+        #plt.plot(rpms, P_actual, c='black', linestyle='--', label='Available power')
+        plt.xlabel('rpm')
+        plt.ylim([10, 60])
+        plt.ylabel('Power required cruise')
+
+        bat1 = (22/5000)*rpms
+        bat2 = (32/4000)*rpms
+        bat3 = (52/4000)*rpms
+
+        plt.plot(rpms, bat1, label='bat 1', linestyle =':')
+        plt.plot(rpms, bat2, label='bat 2', linestyle =':')
+        plt.plot(rpms, bat3, label='bat 3', linestyle =':')
+        plt.legend()
+        plt.show()
+
+    def rpm_velocity(self, R, W, omega, rho, D_c, P_a):
+        V_c = np.arange(0, 140, 0.1)
+
+    def power_rpm_hover(self, R, W, omega, rho, D_c, P_a):
+        rpms = np.arange(0, 4100, 100)
+        P = np.array([self.get_power(0, R, W, i, rho, D_c)[0] / (12000*0.7) for i in rpms])
+        bat = (52 / 4000) * rpms
+
+        P_a = bat - P
+
+        plt.plot(rpms, P, label='Power required')
+        plt.plot(rpms, P_a, label='Excess power')
+        plt.plot(rpms, bat, label='Power available')
+        plt.ylim([0, 50])
+        plt.xlim([0, 4000])
+        plt.xlabel('\u03C9 (rpm)')
+        plt.ylabel('P (kW)')
+        plt.legend()
+        plt.show()
+
 
     def D_c_P(self, disk_loading, W, omega, rho, D_c, P_a):
         V = np.arange(0, 200/3.6, 1)
@@ -211,11 +290,14 @@ class PowerSensitivity():
         plt.show()
 
     def do_sensitivity_analysis(self, disk_loading, W, omega, rho, D_c, P_a):
-        self.D_c_P(disk_loading, W, omega, rho, D_c, P_a)
-        self.disk_loading_P(disk_loading, W, omega, rho, D_c, P_a)
-        self.rho_P(disk_loading, W, omega, rho, D_c, P_a)
-        self.W_P(disk_loading, W, omega, rho, D_c, P_a)
-        self.omega_P(disk_loading, W, omega, rho, D_c, P_a)
+        self.power_radius(disk_loading, W, omega, rho, D_c, P_a)
+        self.power_rpm_final(disk_loading, W, omega, rho, D_c, P_a)
+        self.power_rpm_hover(disk_loading, W, omega, rho, D_c, P_a)
+        #self.D_c_P(disk_loading, W, omega, rho, D_c, P_a)
+        #self.disk_loading_P(disk_loading, W, omega, rho, D_c, P_a)
+        #self.rho_P(disk_loading, W, omega, rho, D_c, P_a)
+        #self.W_P(disk_loading, W, omega, rho, D_c, P_a)
+        #self.omega_P(disk_loading, W, omega, rho, D_c, P_a)
 
         #self.D_c_T(disk_loading, W, omega, rho, D_c)
         #self.disk_loading_T(disk_loading, W, omega, rho, D_c)
@@ -223,7 +305,7 @@ class PowerSensitivity():
         #self.W_T(disk_loading, W, omega, rho, D_c)
 
 
-#disk_loading, W, omega, rho, D_c
+#R, W, omega, rho, D_c
 sens = PowerSensitivity()
-sens.do_sensitivity_analysis(500, 1000, 1000, 1.225, 400, 15)
+sens.do_sensitivity_analysis(0.9, 1200, 2011.5, 1.225, 350, 29)
 #sens.power_thrust(500, 1500, 5000, 1.225, 400)
